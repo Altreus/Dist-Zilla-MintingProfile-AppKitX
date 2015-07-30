140810ffa247a2ad781da13540ece75fda2ff82c
Alastair McGowan-Douglas <alastair.mcgowan@opusvl.com>
2015-07-30 12:56:36 +0100

    Add AppKitX Plugin role to plugin module

--- share/profiles/default/AppKitX.pm
+++ share/profiles/default/AppKitX.pm
@@ -4,6 +4,8 @@ use Moose::Role;
 use CatalystX::InjectComponent;
 use namespace::autoclean;
 
+with 'OpusVL::AppKit::RolesFor::Plugin';
+
 our $VERSION = '0.01';
 {{ 
     ($controller) = ($name =~ /::([^:]+)$/); ''
